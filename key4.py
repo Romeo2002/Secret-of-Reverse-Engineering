5ae4547582fa9afd81cfd67736b93594058c704f
Romeo2002 <45552843+Romeo2002@users.noreply.github.com>
2020-03-17 11:19:20 +0700

    Update key4.py

--- key4.py
+++ key4.py
@@ -5,17 +5,21 @@ Username = raw_input("Please enter your username:")
 print Username
 Username_len = len(Username)
 print "Username Lenght is: %d" %Username_len
-buffer = int(binascii.hexlify(Username[0:4]),base=16)
+buffer = "".join(reversed(Username[0:4]))
+buffer = int(binascii.hexlify(buffer),base=16)
 print "Buffer %x"%buffer
 if len(Username) <=1:
 	i =1
 	print int(binascii.hexlify(key[i]), base=16)
 	EBX = (buffer - int(binascii.hexlify(key[i]), base=16))*int(binascii.hexlify(key[i]), base=16) 
+	EBX = EBX & 0xffffffff
 	print "%x"%EBX
 	ESI = EBX
 	EBX = EBX - (i)
 	EBX = EBX + 0x4353543
+	EBX = EBX & 0xffffffff
 	ESI = ESI + EBX
+	ESI = ESI & 0xffffffff
 	password = ESI^int(binascii.hexlify(key[i]),base=16)
 	print "%x"%password
 	print "%d"%password
@@ -23,12 +27,14 @@ else:
 	i = 4
 	print int(binascii.hexlify(key[i]), base=16)
 	EBX = (buffer - int(binascii.hexlify(key[i]), base=16))*int(binascii.hexlify(key[i]), base=16) 
-	print "%x"%EBX
+	EBX = EBX & 0xffffffff
+	print "EBX %x"%EBX
 	ESI = EBX
 	EBX = EBX - (i)
 	EBX = EBX + 0x4353543
+	EBX = EBX & 0xffffffff
 	ESI = ESI + EBX
+	ESI = ESI & 0xffffffff
 	password = ESI^int(binascii.hexlify(key[i]),base=16)
 	print "%x"%password
 	print "%d"%password
-
